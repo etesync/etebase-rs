16e2cef917904eac3f02bf1051f60d9b2ae87fdb
Tom Hacohen <tom@stosb.com>
2020-03-23 11:34:13 +0200

    snap

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,12 +1,15 @@
 extern crate openssl;
 
 use openssl::pkcs5::scrypt;
+use openssl::error::ErrorStack;
 
-pub fn derive_key(salt: &str, password: &str) -> Result<Box<[u8]>, ()> {
+// FIXME: have our own error
+pub fn derive_key(salt: &str, password: &str) -> Result<Box<[u8]>, ErrorStack> {
     let salt = salt.as_bytes();
     let password = password.as_bytes();
     let mut key: Box<[u8]> = Box::new([0; 190]);
-    // FIXME: we shouldn't be unwrapping!
-    scrypt(password, salt, 16384, 8, 1, 0, &mut *key).unwrap();
-    Ok(key)
+    match scrypt(password, salt, 16384, 8, 1, 0, &mut *key) {
+        Ok(_res) => Ok(key),
+        Err(e) => Err(e),
+    }
 }
