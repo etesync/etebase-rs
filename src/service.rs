38ad4bc301db012392864ab2c519fb0e30589fd0
Tom Hacohen <tom@stosb.com>
2020-03-23 21:01:21 +0200

    Implement journal manager

--- src/service.rs
+++ src/service.rs
@@ -1,6 +1,6 @@
 use url::{Url, ParseError};
 
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
 use reqwest::{
     blocking::Client,
@@ -21,6 +21,22 @@ pub fn get_client() -> Result<Client, Box<dyn std::error::Error>> {
         .build()?;
 
     Ok(client)
+}
+
+pub fn test_reset(client: &Client, auth_token: &str, api_base: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let api_base = Url::parse(api_base)?;
+    let url = api_base.join("reset/")?;
+
+    let res = client.post(url.as_str())
+        .header(header::AUTHORIZATION, format!("Token {}", auth_token))
+        .send()?;
+
+    match res.error_for_status() {
+        Ok(_res) => Ok(()),
+        Err(err) => {
+            Err(Box::new(err))
+        }
+    }
 }
 
 pub struct Authenticator<'a> {
@@ -62,5 +78,192 @@ impl Authenticator<'_> {
             .send()?;
 
         Ok(res.text()?)
+    }
+}
+
+fn get_base_headers(auth_token: &str, capacity: usize) -> header::HeaderMap<header::HeaderValue> {
+    let capacity = capacity + 3;
+    let mut map = header::HeaderMap::with_capacity(capacity);
+    map.insert(header::CONTENT_TYPE, "application/json;charset=UTF-8".parse().unwrap());
+    map.insert(header::ACCEPT, "application/json".parse().unwrap());
+    map.insert(header::AUTHORIZATION, format!("Token {}", auth_token).parse().unwrap());
+
+    map
+}
+
+#[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
+struct JournalJson {
+    uid: String,
+    version: u8,
+    owner: String,
+    content: String,
+    readOnly: Option<bool>,
+    key: Option<String>,
+    lastUid: Option<String>,
+}
+
+#[derive(Clone)]
+pub struct Journal {
+    pub uid: String,
+    pub version: u8,
+    pub owner: String,
+    pub content: Vec<u8>,
+    pub read_only: bool,
+    pub key: Option<Vec<u8>>,
+    pub last_uid: Option<String>,
+}
+
+impl Journal {
+    // FIXME: this should return a result
+    fn from_json(uid: &str, json: &JournalJson) -> Journal {
+        #[allow(non_snake_case)]
+        Journal {
+            uid: uid.to_owned(),
+            version: json.version,
+            owner: json.owner.clone(),
+            read_only: match json.readOnly {
+                Some(val) => val,
+                None => false,
+            },
+            content: base64::decode(&json.content).unwrap(),
+            key: match &json.key {
+                Some(val) => Some(base64::decode(val).unwrap()),
+                None => None,
+            },
+            last_uid: json.lastUid.clone(),
+        }
+    }
+
+    fn to_json(&self) -> JournalJson {
+        JournalJson {
+            uid: self.uid.clone(),
+            version: self.version,
+            owner: self.owner.clone(),
+            readOnly: Some(self.read_only),
+            content: base64::encode(&self.content),
+            key: match &self.key {
+                Some(val) => Some(base64::encode(val)),
+                None => None,
+            },
+            lastUid: None,
+        }
+    }
+
+    pub fn get_crypto_manager() {
+    }
+}
+
+pub struct JournalManager<'a> {
+    api_base: Url,
+    client: &'a Client,
+    auth_token: String,
+}
+
+impl JournalManager<'_> {
+    pub fn new<'a>(client: &'a Client, auth_token: &str, api_base: &str) -> JournalManager<'a> {
+        let api_base = Url::parse(api_base).unwrap();
+        let api_base = api_base.join("api/v1/journals/").unwrap();
+        JournalManager {
+            api_base,
+            client,
+            auth_token: auth_token.to_owned(),
+        }
+    }
+
+    pub fn fetch(&self, journal_uid: &str) -> Result<Journal, Box<dyn std::error::Error>> {
+        let url = self.api_base.join(&format!{"{}/", journal_uid})?;
+        let headers = get_base_headers(&self.auth_token, 0);
+        let res = self.client.get(url.as_str())
+            .headers(headers)
+            .send()?;
+
+        match res.error_for_status() {
+            Ok(res) => {
+                let journal_json = res.json::<JournalJson>()?;
+                Ok(Journal::from_json(journal_uid, &journal_json))
+            },
+            Err(err) => {
+                Err(Box::new(err))
+            }
+        }
+    }
+
+    pub fn list(&self) -> Result<Vec<Journal>, Box<dyn std::error::Error>> {
+        let url = &self.api_base;
+        let headers = get_base_headers(&self.auth_token, 0);
+        let res = self.client.get(url.as_str())
+            .headers(headers)
+            .send()?;
+
+        match res.error_for_status() {
+            Ok(res) => {
+                let journals_json = res.json::<Vec<JournalJson>>()?;
+                Ok(journals_json.into_iter().map(|journal_json| Journal::from_json(&journal_json.uid, &journal_json)).collect())
+            },
+            Err(err) => {
+                Err(Box::new(err))
+            }
+        }
+    }
+
+    pub fn create(&self, journal: &Journal) -> Result<(), Box<dyn std::error::Error>> {
+        let url = &self.api_base;
+        let headers = get_base_headers(&self.auth_token, 0);
+
+        let journal_json = journal.to_json();
+
+        let res = self.client.post(url.as_str())
+            .headers(headers)
+            .json(&journal_json)
+            .send()?;
+
+        match res.error_for_status() {
+            Ok(_res) => {
+                Ok(())
+            },
+            Err(err) => {
+                Err(Box::new(err))
+            }
+        }
+    }
+
+    pub fn update(&self, journal: &Journal) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.api_base.join(&format!{"{}/", &journal.uid})?;
+        let headers = get_base_headers(&self.auth_token, 0);
+
+        let journal_json = journal.to_json();
+
+        let res = self.client.put(url.as_str())
+            .headers(headers)
+            .json(&journal_json)
+            .send()?;
+
+        match res.error_for_status() {
+            Ok(_res) => {
+                Ok(())
+            },
+            Err(err) => {
+                Err(Box::new(err))
+            }
+        }
+    }
+
+    pub fn delete(&self, journal: &Journal) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.api_base.join(&format!{"{}/", &journal.uid})?;
+        let headers = get_base_headers(&self.auth_token, 0);
+
+        let res = self.client.delete(url.as_str())
+            .headers(headers)
+            .send()?;
+
+        match res.error_for_status() {
+            Ok(_res) => {
+                Ok(())
+            },
+            Err(err) => {
+                Err(Box::new(err))
+            }
+        }
     }
 }
--- tests/service.rs
+++ tests/service.rs
@@ -1,7 +1,10 @@
 use etesync::{
     service::{
+        test_reset,
         get_client,
         Authenticator,
+        JournalManager,
+        Journal,
     },
 };
 
@@ -15,6 +18,57 @@ fn auth_token() {
     let client = get_client().unwrap();
     let authenticator = Authenticator::new(&client, TEST_API_URL);
     let token = authenticator.get_token(USER, PASSWORD).unwrap();
+
+    authenticator.invalidate_token(&token).unwrap();
+}
+
+#[test]
+fn simple_sync() {
+    let client = get_client().unwrap();
+    let authenticator = Authenticator::new(&client, TEST_API_URL);
+    let token = authenticator.get_token(USER, PASSWORD).unwrap();
+    test_reset(&client, &token, TEST_API_URL).unwrap();
+
+    let journal_manager = JournalManager::new(&client, &token, TEST_API_URL);
+
+    let mut journal = Journal {
+        uid: String::from("f3436f50b2f7f1613ad142dbce1d24801d9daaabc45ecb2db909251a214c9841"),
+        version: 2,
+        owner: String::from(""),
+        content: b"bla".to_vec(),
+        read_only: false,
+        key: None,
+        last_uid: None,
+    };
+
+    {
+        journal_manager.create(&journal).unwrap();
+        let journal2 = journal_manager.fetch(&journal.uid).unwrap();
+
+        assert_eq!(journal.content, journal2.content);
+    }
+
+    assert_eq!(journal_manager.list().unwrap().len(), 1);
+
+    {
+        journal.content = b"bla2".to_vec();
+
+        journal_manager.update(&journal).unwrap();
+        let journal2 = journal_manager.fetch(&journal.uid).unwrap();
+        assert_eq!(journal.content, journal2.content);
+    }
+
+    {
+        journal.uid = String::from("f3436f50b2f7f1613ad142dbce1d24801d9daaabc45ecb2db909251a20000000");
+        journal.content = b"blabla".to_vec();
+
+        journal_manager.create(&journal).unwrap();
+        assert_eq!(journal_manager.list().unwrap().len(), 2);
+    }
+
+    journal_manager.delete(&journal).unwrap();
+
+    assert_eq!(journal_manager.list().unwrap().len(), 1);
 
     authenticator.invalidate_token(&token).unwrap();
 }
