936bce5e05bfb86c24355495d407118d600df2ab
Tom Hacohen <tom@stosb.com>
2020-07-21 09:53:43 +0300

    Encrypted models: implement EncryptedItem.

--- src/encrypted_models.rs
+++ src/encrypted_models.rs
@@ -9,6 +9,7 @@ use serde::{Serialize, Deserialize};
 
 use super::{
     try_into,
+    CURRENT_VERSION,
     crypto::{
         CryptoManager,
         CryptoMac,
@@ -31,6 +32,42 @@ use super::{
 
 pub fn gen_uid_base64() -> StringBase64 {
   return to_base64(&randombytes(24)).unwrap();
+}
+
+pub struct AccountCryptoManager(CryptoManager);
+
+impl AccountCryptoManager {
+    pub fn new(key: &[u8; 32], version: u8) -> Result<Self> {
+        let context = b"Acct    ";
+
+        Ok(Self {
+            0: CryptoManager::new(key, &context, version)?,
+        })
+    }
+}
+
+pub struct CollectionCryptoManager(CryptoManager);
+
+impl CollectionCryptoManager {
+    pub fn new(key: &[u8; 32], version: u8) -> Result<Self> {
+        let context = b"Col     ";
+
+        Ok(Self {
+            0: CryptoManager::new(key, &context, version)?,
+        })
+    }
+}
+
+pub struct ItemCryptoManager(CryptoManager);
+
+impl ItemCryptoManager {
+    pub fn new(key: &[u8; 32], version: u8) -> Result<Self> {
+        let context = b"ColItem ";
+
+        Ok(Self {
+            0: CryptoManager::new(key, &context, version)?,
+        })
+    }
 }
 
 pub type CollectionSerialWrite = EncryptedCollection;
@@ -72,7 +109,7 @@ pub struct EncryptedRevision {
 }
 
 impl EncryptedRevision {
-    pub fn new(crypto_manager: &CryptoManager, additional_data: &[u8], meta: &[u8], content: &[u8]) -> Result<Self> {
+    pub fn new(crypto_manager: &ItemCryptoManager, additional_data: &[u8], meta: &[u8], content: &[u8]) -> Result<Self> {
         let mut ret = Self {
             uid: "".to_owned(),
             meta: vec![],
@@ -95,8 +132,8 @@ impl EncryptedRevision {
         self
     }
 
-    fn calculate_hash(&self, crypto_manager: &CryptoManager, additional_data: &[u8]) -> Result<Vec<u8>> {
-        let mut crypto_mac = crypto_manager.get_crypto_mac()?;
+    fn calculate_hash(&self, crypto_manager: &ItemCryptoManager, additional_data: &[u8]) -> Result<Vec<u8>> {
+        let mut crypto_mac = crypto_manager.0.get_crypto_mac()?;
         crypto_mac.update(&[self.deleted as u8])?;
         crypto_mac.update_with_len_prefix(additional_data)?;
 
@@ -112,18 +149,18 @@ impl EncryptedRevision {
         crypto_mac.finalize()
     }
 
-    pub fn verify(&self, crypto_manager: &CryptoManager, additional_data: &[u8]) -> Result<bool> {
+    pub fn verify(&self, crypto_manager: &ItemCryptoManager, additional_data: &[u8]) -> Result<bool> {
         let mac = from_base64(&self.uid)?;
         let ad_hash = self.calculate_hash(crypto_manager, additional_data)?;
 
-        crypto_manager.verify(&self.meta, try_into!(&mac[..])?, Some(&ad_hash))
+        crypto_manager.0.verify(&self.meta, try_into!(&mac[..])?, Some(&ad_hash))
     }
 
-    pub fn set_meta(&mut self, crypto_manager: &CryptoManager, additional_data: &[u8], meta: &[u8]) -> Result<()> {
+    pub fn set_meta(&mut self, crypto_manager: &ItemCryptoManager, additional_data: &[u8], meta: &[u8]) -> Result<()> {
         let ad_hash = self.calculate_hash(crypto_manager, additional_data)?;
 
         let msg = buffer_pad_meta(meta)?;
-        let enc_content = crypto_manager.encrypt_detached(&msg, Some(&ad_hash))?;
+        let enc_content = crypto_manager.0.encrypt_detached(&msg, Some(&ad_hash))?;
 
         self.uid = to_base64(&enc_content.0)?;
         self.meta = enc_content.1;
@@ -131,14 +168,14 @@ impl EncryptedRevision {
         Ok(())
     }
 
-    pub fn decrypt_meta(&self, crypto_manager: &CryptoManager, additional_data: &[u8]) -> Result<Vec<u8>> {
+    pub fn decrypt_meta(&self, crypto_manager: &ItemCryptoManager, additional_data: &[u8]) -> Result<Vec<u8>> {
         let mac = from_base64(&self.uid)?;
         let ad_hash = self.calculate_hash(crypto_manager, additional_data)?;
 
-        buffer_unpad(&crypto_manager.decrypt_detached(&self.meta, try_into!(&mac[..])?, Some(&ad_hash))?)
+        buffer_unpad(&crypto_manager.0.decrypt_detached(&self.meta, try_into!(&mac[..])?, Some(&ad_hash))?)
     }
 
-    pub fn set_content(&mut self, crypto_manager: &CryptoManager, additional_data: &[u8], content: &[u8]) -> Result<()> {
+    pub fn set_content(&mut self, crypto_manager: &ItemCryptoManager, additional_data: &[u8], content: &[u8]) -> Result<()> {
         let meta = self.decrypt_meta(crypto_manager, additional_data)?;
 
         let mut chunks: Vec<ChunkArrayItem> = vec![];
@@ -153,7 +190,7 @@ impl EncryptedRevision {
 
         if chunk_start < content.len() {
             let buf = &content[chunk_start..];
-            let hash = to_base64(&crypto_manager.calculate_mac(buf)?)?;
+            let hash = to_base64(&crypto_manager.0.calculate_mac(buf)?)?;
             chunks.push((hash, Some(buf.to_vec())));
         }
 
@@ -164,7 +201,7 @@ impl EncryptedRevision {
         let encrypt_item = |item: ChunkArrayItem| -> Result<ChunkArrayItem> {
             let (hash, buf) = item;
             let ret = match buf {
-                Some(buf) => Some(crypto_manager.encrypt(&buffer_pad(&buf)?, None)?),
+                Some(buf) => Some(crypto_manager.0.encrypt(&buffer_pad(&buf)?, None)?),
                 None => None,
             };
 
@@ -183,16 +220,16 @@ impl EncryptedRevision {
         Ok(())
     }
 
-    pub fn decrypt_content(&self, crypto_manager: &CryptoManager, additional_data: &[u8]) -> Result<Vec<u8>> {
+    pub fn decrypt_content(&self, crypto_manager: &ItemCryptoManager, additional_data: &[u8]) -> Result<Vec<u8>> {
         let decrypt_item = |item: &ChunkArrayItem| -> Result<Vec<u8>> {
             let (hash_str, buf) = item;
             let buf = match buf {
-                Some(buf) => buffer_unpad(&crypto_manager.decrypt(&buf, None)?)?,
+                Some(buf) => buffer_unpad(&crypto_manager.0.decrypt(&buf, None)?)?,
                 None => return Err(Error::Generic("Got chunk without data".to_owned())),
             };
 
             let hash = from_base64(&hash_str)?;
-            let calculated_mac = crypto_manager.calculate_mac(&buf)?;
+            let calculated_mac = crypto_manager.0.calculate_mac(&buf)?;
 
             if !memcmp(&hash, &calculated_mac) {
                 return Err(Error::Integrity(format!("Got a wrong mac for chunk {}", hash_str)));
@@ -216,12 +253,145 @@ impl EncryptedRevision {
         ret
     }
 
-    pub fn delete(&mut self, crypto_manager: &CryptoManager, additional_data: &[u8]) -> Result<()> {
+    pub fn delete(&mut self, crypto_manager: &ItemCryptoManager, additional_data: &[u8]) -> Result<()> {
         let meta = self.decrypt_meta(crypto_manager, additional_data)?;
 
         self.deleted = true;
 
         self.set_meta(crypto_manager, additional_data, &meta)?;
         Ok(())
+    }
+}
+
+pub type ItemSerialWrite = EncryptedItem;
+pub type ItemSerialRead = EncryptedItem;
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct EncryptedItem {
+    uid: StringBase64,
+    version: u8,
+
+    #[serde(with = "serde_bytes")]
+    encryption_key: Option<Vec<u8>>,
+    content: EncryptedRevision,
+
+    etag: Option<String>,
+}
+
+impl EncryptedItem {
+    pub fn new(parent_crypto_manager: &CollectionCryptoManager, meta: &[u8], content: &[u8]) -> Result<Self> {
+        let uid = gen_uid_base64();
+        let version = CURRENT_VERSION;
+        let crypto_manager = Self::get_crypto_manager_static(parent_crypto_manager, &uid, version, None)?;
+        let content = EncryptedRevision::new(&crypto_manager, Self::get_additional_mac_data_static(&uid), meta, content)?;
+
+        Ok(Self {
+            uid,
+            version,
+            encryption_key: None,
+            content,
+
+            etag: None,
+        })
+    }
+
+    pub fn deserialize(serialized: ItemSerialRead) -> Self {
+        serialized
+    }
+
+    pub fn serialize(&self) -> &ItemSerialWrite {
+        self
+    }
+
+    pub(crate) fn mark_saved(&mut self) {
+        self.etag = Some(self.content.uid.clone());
+    }
+
+    pub fn is_locally_changed(&self) -> bool {
+        match self.etag.as_deref() {
+            Some(etag) => etag == self.content.uid,
+            None => false,
+        }
+    }
+
+    pub fn verify(&self, crypto_manager: &ItemCryptoManager) -> Result<bool> {
+        self.content.verify(crypto_manager, self.get_additional_mac_data())
+    }
+
+    // FIXME: meta should be a special struct that we have that we let people manipulate
+    pub fn set_meta(&mut self, crypto_manager: &ItemCryptoManager, meta: &[u8]) -> Result<()> {
+        let ad_mac_data = Self::get_additional_mac_data_static(&self.uid);
+        if self.is_locally_changed() {
+            self.content.set_meta(crypto_manager, ad_mac_data, meta)?;
+        } else {
+            let mut rev = self.content.clone();
+            rev.set_meta(crypto_manager, ad_mac_data, meta)?;
+            self.content = rev;
+        };
+
+        Ok(())
+    }
+
+    pub fn decrypt_meta(&self, crypto_manager: &ItemCryptoManager) -> Result<Vec<u8>> {
+        self.verify(crypto_manager)?;
+        self.content.decrypt_meta(crypto_manager, self.get_additional_mac_data())
+    }
+
+    pub fn set_content(&mut self, crypto_manager: &ItemCryptoManager, content: &[u8]) -> Result<()> {
+        let ad_mac_data = Self::get_additional_mac_data_static(&self.uid);
+        if self.is_locally_changed() {
+            self.content.set_content(crypto_manager, ad_mac_data, content)?;
+        } else {
+            let mut rev = self.content.clone();
+            rev.set_content(crypto_manager, ad_mac_data, content)?;
+            self.content = rev;
+        };
+
+        Ok(())
+    }
+
+    pub fn decrypt_content(&self, crypto_manager: &ItemCryptoManager) -> Result<Vec<u8>> {
+        self.verify(crypto_manager)?;
+        self.content.decrypt_content(crypto_manager, self.get_additional_mac_data())
+    }
+
+    pub fn delete(&mut self, crypto_manager: &ItemCryptoManager) -> Result<()> {
+        let ad_mac_data = Self::get_additional_mac_data_static(&self.uid);
+        if self.is_locally_changed() {
+            self.content.delete(crypto_manager, ad_mac_data)?;
+        } else {
+            let mut rev = self.content.clone();
+            rev.delete(crypto_manager, ad_mac_data)?;
+            self.content = rev;
+        };
+
+        Ok(())
+    }
+
+    pub fn is_deleted(&self) -> bool {
+        self.content.deleted
+    }
+
+    fn get_crypto_manager_static(parent_crypto_manager: &CollectionCryptoManager, uid: &str, version: u8, encryption_key: Option<&[u8]>) -> Result<ItemCryptoManager> {
+        let encryption_key = match encryption_key {
+            Some(encryption_key) => parent_crypto_manager.0.decrypt(encryption_key, None)?,
+            None => parent_crypto_manager.0.derive_subkey(uid.as_bytes())?,
+        };
+
+        ItemCryptoManager::new(try_into!(&encryption_key[..])?, version)
+    }
+
+    pub fn get_crypto_manager(&self, parent_crypto_manager: &CollectionCryptoManager) -> Result<ItemCryptoManager> {
+        let encryption_key = self.encryption_key.as_deref().and_then(|x| Some(&x[..]));
+        Self::get_crypto_manager_static(parent_crypto_manager, &self.uid, self.version, encryption_key)
+    }
+
+    fn get_additional_mac_data_static(uid: &str) -> &[u8] {
+        uid.as_bytes()
+    }
+
+    fn get_additional_mac_data(&self) -> &[u8] {
+        Self::get_additional_mac_data_static(&self.uid)
     }
 }
