18a51007bff4843804d00ac0b3481646456d0029
Tom Hacohen <tom@stosb.com>
2020-07-28 12:40:50 +0300

    Support fetch options.

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -104,3 +104,60 @@ pub fn client_new_with_impl(server_url: &str, imp: Box<dyn HttpClient>) -> Resul
     let imp = Box::new(JavaHttpClient::new(imp));
     Client::new_with_impl(server_url, imp)
 }
+
+pub struct FetchOptions {
+    limit: Option<usize>,
+    stoken: Option<String>,
+    iterator: Option<String>,
+    prefetch: Option<bool>,
+    with_collection: Option<bool>,
+}
+
+impl FetchOptions {
+    pub fn new() -> Self {
+        Self {
+            limit: None,
+            stoken: None,
+            iterator: None,
+            prefetch: None,
+            with_collection: None,
+        }
+    }
+
+    pub fn limit(&mut self, limit: usize) {
+        self.limit = Some(limit);
+    }
+
+    pub fn prefetch(&mut self, prefetch: bool) {
+        self.prefetch = Some(prefetch);
+    }
+
+    pub fn with_collection(&mut self, with_collection: bool) {
+        self.with_collection = Some(with_collection);
+    }
+
+    pub fn iterator(&mut self, iterator: Option<&str>) {
+        self.iterator = iterator.map(str::to_string);
+    }
+
+    pub fn stoken(&mut self, stoken: Option<&str>) {
+        self.stoken = stoken.map(str::to_string);
+    }
+
+    pub fn to_fetch_options<'a>(&'a self) -> etebase::FetchOptions<'a> {
+        let mut ret = etebase::FetchOptions::new();
+        if let Some(limit) = self.limit {
+            ret = ret.limit(limit);
+        }
+        if let Some(prefetch) = self.prefetch {
+            ret = ret.prefetch(prefetch);
+        }
+        if let Some(with_collection) = self.with_collection {
+            ret = ret.with_collection(with_collection);
+        }
+        ret = ret.iterator(self.iterator.as_deref());
+        ret = ret.stoken(self.stoken.as_deref());
+        ret
+    }
+}
+
